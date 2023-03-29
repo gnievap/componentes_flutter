14d7881000473545a3ba726a786ed0433a5b38e1
Gaby Nieva <gaby.nieva@outlook.com>
2023-03-29 00:17:18 -0600

    Iniciando con el theme

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,17 @@ class MyApp extends StatelessWidget {
       //home: const CardScreen(),
       initialRoute: AppRoutes.initialRoute,
       routes: AppRoutes.getAppRoutes(),
-      onGenerateRoute: AppRoutes.onGenerateRoute
+      onGenerateRoute: AppRoutes.onGenerateRoute,
+      theme: ThemeData.light().copyWith(
+        // Color primario
+        primaryColor : Colors.indigo,
+        // AppBar Theme
+        appBarTheme: const AppBarTheme(
+          color: Colors.indigo,
+          elevation: 0,
+        ),
+
+      ),
     );
   }
 }
